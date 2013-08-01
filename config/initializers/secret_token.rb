fec4213b4f16375f7cfcf3f14f441ab0237d11ab
vinaybl7 <vinaybl@gmail.com>
2013-07-31 22:06:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cbaby3::Application.config.secret_key_base = 'd20ca7459a08aadfc36704d7e9f6e2e4587a2bdfb8b7fde9012afcd378454bef96435317038f974eb3ba4ca875492831e21006acf0fe0a7b9b407441761541f0'
